cb51b96257ed725c97fab94a79c059a710c2786f
Silas Stephens <76085257+silasstephens15@users.noreply.github.com>
2025-10-05 19:05:37 -0400

    Added insert and find.
    Find will also be used for the deletion method.

--- main.js
+++ main.js
@@ -18,6 +18,60 @@ class Tree {
   newNode(value) {
     return { value, right: null, left: null };
   }
+  insert(value, node = this.root) {
+    if (node.value > value) {
+      if (node.left !== null) {
+        this.insert(value, node.left);
+      } else {
+        node.left = this.newNode(value);
+        this.arr.push(value);
+      }
+    } else {
+      if (node.right !== null) {
+        this.insert(value, node.right);
+      } else {
+        node.right = this.newNode(value);
+        this.arr.push(value);
+      }
+    }
+  }
+  deleteItem(value) {
+    const node = this.find(value, true);
+    if (node.right === null && node.left === null) {
+    }
+
+    this.arr.splice(this.arr.indexOf(value), 1);
+  }
+  find(value, returnParent = false, node = this.root, parent = {}) {
+    let result = {};
+    if (node.value === value) {
+      return node;
+    }
+    if (node.value > value) {
+      if (node.left !== null) {
+        parent.value = node.value;
+        parent.left = node.left;
+        parent.right = node.right;
+        result = this.find(value, false, node.left, parent);
+      } else {
+        return null;
+      }
+    } else {
+      if (node.right !== null) {
+        parent.value = node.value;
+        parent.left = node.left;
+        parent.right = node.right;
+        result = this.find(value, false, node.right, parent);
+      } else {
+        return null;
+      }
+    }
+    if (returnParent) {
+      return { result, parent };
+    } else {
+      return result;
+    }
+  }
   prettyPrint(node = this.root, prefix = "", isLeft = true) {
     if (node === null) {
       return;
@@ -38,3 +92,4 @@ class Tree {
 
 const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
 tree.prettyPrint();
+console.log(tree.find(7, true));
