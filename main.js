2e50932e8f54ac2f0c5c73d9443aefe4d97da816
Silas Stephens <76085257+silasstephens15@users.noreply.github.com>
2025-10-05 17:19:25 -0400

    Added code to build tree from unsorted array.
    The tree is now constructed properly. The prettyPrint method is adapted from Odin Project code.

--- main.js
+++ main.js
@@ -1,1 +1,40 @@
 import mergeSort from "./merge-sort.js";
+
+class Tree {
+  constructor(arr) {
+    this.arr = mergeSort(arr);
+    this.root = this.buildTree(this.arr, 0, this.arr.length - 1);
+  }
+  buildTree(arr, start, end) {
+    if (start > end) {
+      return null;
+    }
+    let mid = start + Math.floor((end - start) / 2);
+    let root = this.newNode(arr[mid]);
+    root.left = this.buildTree(arr, start, mid - 1);
+    root.right = this.buildTree(arr, mid + 1, end);
+    return root;
+  }
+  newNode(value) {
+    return { value, right: null, left: null };
+  }
+  prettyPrint(node = this.root, prefix = "", isLeft = true) {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      this.prettyPrint(
+        node.right,
+        `${prefix}${isLeft ? "│   " : "    "}`,
+        false
+      );
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
+    if (node.left !== null) {
+      this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  }
+}
+
+const tree = new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+tree.prettyPrint();
--- merge-sort.js
+++ merge-sort.js
@@ -5,6 +5,9 @@ function mergeSort(arr, sorted = [], compare1 = [], compare2 = []) {
   compare1 = mergeSort(arr.slice(0, Math.round(arr.length / 2)));
   compare2 = mergeSort(arr.slice(Math.round(arr.length / 2), arr.length));
   while (compare1.length !== 0 && compare2.length !== 0) {
+    if (compare1[0] === compare2[0]) {
+      compare1.shift();
+    }
     if (compare1[0] < compare2[0]) {
       sorted.push(compare1[0]);
       compare1.shift();
